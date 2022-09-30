b2fb35c99e5f1390050e111b512d469462ebc06d
GreatWyrm <alecmusante@gmail.com>
2022-09-30 16:20:47 -0700

    Fix build errors and remove notes file

--- modules/build.gradle.kts
+++ modules/build.gradle.kts
@@ -16,11 +16,18 @@ subprojects {
     dependencies {
         // A bug with kotlin dsl
         val implementation by configurations
+        val annotationProcessor by configurations
         val testImplementation by configurations
+        val testAnnotationProcessor by configurations
         val errorprone by configurations
 
         errorprone("com.google.errorprone:error_prone_core:2.14.0")
         errorprone("com.uber.nullaway:nullaway:0.9.8")
+
+        // Auto service (SPI)
+        annotationProcessor("com.google.auto.service:auto-service:1.0.1")
+        testAnnotationProcessor("com.google.auto.service:auto-service:1.0.1")
+        implementation("com.google.auto.service:auto-service-annotations:1.0.1")
 
         // Minestom
         implementation("com.github.minestommmo:Minestom:c6c97162a6")
