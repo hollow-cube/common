074075a47dea1ca17b187ea20503b8d007c3fdbd
Matt Worzala <35708499+mworzala@users.noreply.github.com>
2022-10-12 07:16:56 -0400

    Schematic loader (#6)
    * Copy schematic files from libmmo branch and do some cleanup
    
    * add publishing to all modules (jitpack should work now)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,9 @@
 rootProject.name = "common"
 
 include(":modules")
+// Common + Modules which depend on it
 include(":modules:common")
 include(":modules:dev")
+// Standalone modules
+include(":modules:schem")
 include(":modules:test")
