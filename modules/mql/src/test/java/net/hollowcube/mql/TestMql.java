9b37d94466d3377a670c1371c0ac244fd55392be
mworzala <mattheworzala@gmail.com>
2022-10-13 19:22:18 -0400

    add some more tests, slightly reorganize

--- modules/mql/src/test/java/net/hollowcube/mql/TestMql.java
+++ modules/mql/src/test/java/net/hollowcube/mql/TestMql.java
@@ -1,13 +1,43 @@
+package net.hollowcube.mql;
+
 import net.hollowcube.mql.parser.MqlParser;
+import net.hollowcube.mql.runtime.MqlRuntimeError;
+import net.hollowcube.mql.runtime.MqlScope;
 import net.hollowcube.mql.runtime.MqlScopeImpl;
 import net.hollowcube.mql.runtime.MqlScriptScope;
+import net.hollowcube.mql.value.MqlHolder;
 import net.hollowcube.mql.value.MqlNumberValue;
+import net.hollowcube.mql.value.MqlValue;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ParseSimple {
+public class TestMql {
+    // This is a weird test case
+
+    @Test
+    public void basicQueryCall() {
+        var source = "q.is_alive";
+        var expr = new MqlParser(source).parse();
+
+        var scope = new MqlScope() {
+            @Override
+            public @NotNull MqlValue get(@NotNull String name) {
+                if (!name.equals("q") && !name.equals("query"))
+                    throw new MqlRuntimeError("unknown environment object: " + name);
+                return (MqlHolder) queryFunction -> switch (queryFunction) {
+                    case "is_alive" -> new MqlNumberValue(1);
+                    default -> throw new MqlRuntimeError("no such query function: " + queryFunction);
+                };
+            }
+        };
+        var result = expr.evaluate(scope);
+        assertTrue(result instanceof MqlNumberValue);
+        assertEquals(1, ((MqlNumberValue) result).value());
+    }
+
     @Test
     public void testAdd() {
         var source = "10 + 10 + 10";
@@ -84,6 +114,22 @@ public class ParseSimple {
     }
 
     @Test
+    public void testBracketRecursive() {
+        var source = "((((1))))";
+
+        var expr = new MqlParser(source).parse();
+
+        var scopeImpl = new MqlScopeImpl();
+        var vars = new MqlScopeImpl.Mutable();
+
+        var scope = new MqlScriptScope(scopeImpl, vars, scopeImpl);
+        var result = expr.evaluate(scope);
+
+        assertTrue(result instanceof MqlNumberValue);
+        assertEquals(1, ((MqlNumberValue) result).value());
+    }
+
+    @Test
     public void testBrackets() {
         var source = "(10 + 3) * (3 - 1) - 5 + (1 - 10)";
 
@@ -114,4 +160,5 @@ public class ParseSimple {
         assertTrue(result instanceof MqlNumberValue);
         assertEquals(((10 + (3)) * (3 - 1)) - 5 + ((2 * 3) - 10) * (10 + (10 * (3 + 3))), ((MqlNumberValue) result).value());
     }
+
 }
