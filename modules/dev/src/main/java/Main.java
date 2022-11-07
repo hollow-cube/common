b18d9ae534b70ed0d2ae2d85e226a8ab7b8536b8
Nixotica <nixotica@gmail.com>
2022-11-06 21:18:44 -0800

    Adding world manager commands for database interaction and also an actual player spawn lol

--- modules/dev/src/main/java/Main.java
+++ modules/dev/src/main/java/Main.java
@@ -1,7 +1,15 @@
 import net.hollowcube.WorldManager;
 import net.hollowcube.world.World;
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.PlayerLoginEvent;
 import net.minestom.server.extras.MojangAuth;
+import net.minestom.server.instance.Instance;
+import net.minestom.server.instance.InstanceContainer;
+import net.minestom.server.instance.InstanceManager;
+import net.minestom.server.instance.block.Block;
 
 import java.sql.*;
 
@@ -11,6 +19,20 @@ public class Main {
         MinecraftServer server = MinecraftServer.init();
         MojangAuth.init();
         server.start("0.0.0.0", 25565);
+
+        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+        // Create the instance
+        InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
+        // Set the ChunkGenerator
+        instanceContainer.setGenerator(unit ->
+                unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK));
+        // Add an event callback to specify the spawning instance (and the spawn position)
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(PlayerLoginEvent.class, event -> {
+            final Player player = event.getPlayer();
+            event.setSpawningInstance(instanceContainer);
+            player.setRespawnPoint(new Pos(0, 42, 0));
+        });
 
         WorldManager.init();
     }
--- modules/instances/src/main/java/net/hollowcube/commands/world/CopyWorldCommand.java
+++ modules/instances/src/main/java/net/hollowcube/commands/world/CopyWorldCommand.java
@@ -1,4 +1,18 @@
 package net.hollowcube.commands.world;
 
-public class CopyWorldCommand {
+import net.minestom.server.command.builder.Command;
+import net.minestom.server.command.builder.arguments.ArgumentType;
+
+public class CopyWorldCommand extends Command {
+    public CopyWorldCommand() {
+        super("copy", "cp");
+
+        var idArg = ArgumentType.String("id");
+
+        addSyntax((sender, context) -> {
+            final String id = context.get(idArg);
+            // TODO duplicate the world in world manager's database, do not store locally
+            // TODO spit out the new id in console of copied world
+        }, idArg);
+    }
 }
