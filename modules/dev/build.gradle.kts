aeb5bbec3cbb5ccc876fdd7381075faa9125d3f7
mworzala <mattheworzala@gmail.com>
2022-11-18 23:21:13 -0500

    add minestom as implementation dependency in dev module

--- modules/dev/build.gradle.kts
+++ modules/dev/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
     implementation("org.postgresql:postgresql:42.5.0")
     implementation(project(":modules:instances"))
+    implementation("com.github.minestommmo:Minestom:c6c97162a6")
 }
