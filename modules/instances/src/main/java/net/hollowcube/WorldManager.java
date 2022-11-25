35e791654d2dc6b7476ce1d5f49b343641260b84
Nixotica <36284986+Nixotica@users.noreply.github.com>
2022-11-24 23:18:07 -0500

    Connection refused: no further information

--- modules/instances/src/main/java/net/hollowcube/WorldManager.java
+++ modules/instances/src/main/java/net/hollowcube/WorldManager.java
@@ -7,25 +7,24 @@ import net.minestom.server.MinecraftServer;
 
 public class WorldManager {
     private static WorldManager instance;
+    private static PostgreSQLManager PostgreSQL;
+    private static SeaweedFS seaweedFS;
 
     public static WorldManager getInstance() {
         return instance;
     }
 
+    // TODO init with args (file system, database)
     public static void init() {
         instance = new WorldManager();
+        PostgreSQLManager.init();
+        PostgreSQL = PostgreSQLManager.get();
+        seaweedFS = new SeaweedFS();
+        seaweedFS.connectToSeaweedFs();
         CommandLoader.registerCommands();
     }
 
-    private PostgreSQLManager PostgreSQL;
-    private SeaweedFS seaweedFS;
-
-    // TODO add implementation which links world manager instance to exactly one db and fs
-    private void loadWorldManager() {
-        PostgreSQLManager.init(); // TODO use that database
-        PostgreSQL = PostgreSQLManager.get();
-        seaweedFS = new SeaweedFS(); // TODO use that file system
-        seaweedFS.connectToSeaweedFs();
-        // Register commands in here? Or we can store them separately
+    public PostgreSQLManager getPostgreSQL() {
+        return PostgreSQL;
     }
 }
