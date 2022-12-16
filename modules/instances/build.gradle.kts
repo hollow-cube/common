ac7b54d93f8bce89372a1f9f93caa007acd22499
mworzala <mattheworzala@gmail.com>
2022-12-15 23:41:41 -0500

    import only s3, not the rest of the aws sdk

--- modules/instances/build.gradle.kts
+++ modules/instances/build.gradle.kts
@@ -2,7 +2,7 @@
 dependencies {
     implementation("com.github.luben:zstd-jni:1.5.2-2")
 
-    implementation("com.amazonaws:aws-java-sdk:1.12.353")
+    implementation("com.amazonaws:aws-java-sdk-s3:1.12.364")
     implementation("javax.xml.bind:jaxb-api:2.3.1") // Performance improvement for s3
 
     implementation("net.anumbrella.seaweedfs:seaweedfs-java-client:0.0.2.RELEASE")
