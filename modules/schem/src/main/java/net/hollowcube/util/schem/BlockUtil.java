a3f3f449125caf4f80cec90ac24be46ad2b42cce
Matt Worzala <35708499+mworzala@users.noreply.github.com>
2023-05-06 08:41:49 +0300

    fix schematic writer root tag name (#43)

--- modules/schem/src/main/java/net/hollowcube/util/schem/BlockUtil.java
+++ modules/schem/src/main/java/net/hollowcube/util/schem/BlockUtil.java
@@ -1,13 +1,10 @@
-package net.hollowcube.util;
+package net.hollowcube.util.schem;
 
 import net.minestom.server.instance.block.Block;
 import org.jetbrains.annotations.NotNull;
 
-public final class BlockUtil {
-
-    public static @NotNull Block withType(@NotNull Block block, @NotNull Block type) {
-        return type.withHandler(block.handler()).withNbt(block.nbt());
-    }
+final class BlockUtil {
+    private BlockUtil() {}
 
     public static @NotNull String toStateString(@NotNull Block block) {
         if (block.properties().isEmpty())
